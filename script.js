e5eb8a3a846e8f2d86ea1131eff6e8d9bdeb459f
Joseph Campbell <thecanadianjoe@gmail.com>
2022-03-30 15:44:13 -0400

    added base computer and player choice for game

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-// script.js
-
-// rock, paper, scissors
+const choices = ["rock","paper","scissors"]
 
 
+function game() {
+  playRound();
+}
+
+function playRound(){
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+}
+
+function playerChoice () {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while(input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    while (check == false){
+        input = prompt("Type Rock, Paper, or Scissors. Perfect spelling, please.");
+        while(input == null) {
+            input = prompt("Type Rock, Paper, or Scissors");
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    
+   // console.log(input);
+}
+
+function computerChoice(){
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function validateInput(choice) {
+    return choices.includes(choice);
+    }
+
+ function checkWinner(choiceP, choiceC){
+     if(choiceP === choiceC){
+         return "Tie";
+      } else if ()
+      (choiceP === "rock" && choiceC ==="scissors") || 
+      (choiceP === "paper" && choiceC ==="rock") || 
+      (choiceP === "scissors" && choiceC ==="paper") 
+     ) {
+          return "Player";
+      }
+     
+ }   
+
+    
+
+
+game();
